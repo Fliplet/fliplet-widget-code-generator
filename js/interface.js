d52497ec8e2597f71ec531489d09d0d2837dbac4
Veljko Popović <veljko_popovic_90@hotmail.com>
2025-02-24 10:10:47 +0100

    code generator init

---
+++ js/interface.js
@@ -0,0 +1,16 @@
+Fliplet.Widget.generateInterface({
+  fields: [
+    {
+      name: 'aiModel',
+      type: 'text',
+      label: 'AI model',
+      default: 'gpt-4o'
+    },
+    {
+      name: 'prompt',
+      type: 'textarea',
+      label: 'Prompt',
+      default: ''
+    }
+  ]
+});
