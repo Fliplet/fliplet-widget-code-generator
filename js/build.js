f599cd3cf1a6cb7f5b1d9fffecf991a81b400520
Veljko Popović <veljko_popovic_90@hotmail.com>
2025-02-27 15:02:43 +0100

    ip

--- js/build.js
+++ js/build.js
@@ -3,10 +3,7 @@ Fliplet.Widget.instance({
   name: "code-generator",
   displayName: "Code generator",
   render: {
-    template: [
-      '<div class="code-generator-content">',
-      "</div>",
-    ].join(""),
+    template: ['<div class="code-generator-content">', "</div>"].join(""),
     ready: function () {
       // Initialize children components when this widget is ready
       Fliplet.Widget.initializeChildren(this.$el, this);
@@ -28,31 +25,17 @@ Fliplet.Widget.instance({
 
       debugger;
 
-      // if (
-      //   AI.fields.dataSourceId &&
-      //   AI.fields.prompt
-      // ) {
-        
-      //   $aiContainer.html(`
-      //     <div class="overlay-loading">
-      //       <p>Generating code, please wait...</p>
-      //     </div>
-      //   `);
-
-      //   return queryAI(AI.fields.prompt)
-      //     .then(function (parsedContent) {
-      //       // Save the generated code
-      //       return saveGeneratedCode(parsedContent);
-      //     })
-      //     .then(() => {
-      //       // Reload the page to show the newly generated content
-      //       // window.location.reload();
-      //     })
-      //     .catch(function (error) {
-      //       console.error("Error in process:", error);
-      //       return Promise.reject(error);
-      //     });
+      if (!AI.fields.dataSourceId || !AI.fields.prompt) {
+        Fliplet.UI.toast.error(
+          "Please select a data source and enter a prompt"
+        );
+        return;
       }
+
+      if (AI.fields.css && AI.fields.javascript && AI.fields.layout) {
+        $aiContainer.html(AI.fields.layout);
+      }
+    },
     // },
   },
 });
